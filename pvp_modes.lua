400c75f592f5493aa89102da292d400f95daadbc
InfinityPR <kelvinrosario182@gmail.com>
2025-11-14 20:19:10 -0500

    Define PvP modes with coordinates and vehicles
    Added various PvP modes with details including coordinates, vehicles, and descriptions.

---
+++ pvp_modes.lua
@@ -0,0 +1,46 @@
+-- Name of the arena, interior id, player 1&2 coords (x,y,z) & vehicles if listed
+pvpList = {
+	["1v1 Combat Fight: Battlefield"] = {
+		interior = 10,
+		["P1"] = {x=-974.24554,y=1061.08655,z=1345.67542},
+		["P2"] = {x=-1132.07324,y=1057.67249,z=1346.41235},
+		description = "You will fight against your opponent in ground combat."
+	},
+	["1v1 Hydra Fight"] = {
+		interior = 0,
+		vehicle = 520,
+		["P1"] = {x=-974.24554,y=1061.08655,z=1345.67542},
+		["P2"] = {x=-1132.07324,y=1057.67249,z=1346.41235},
+		description = "You will fight against your chosen opponent in hydra."
+	},
+	["1v1 Hunter Fight"] = {
+		interior = 0,
+		vehicle = 425,
+		["P1"] = {x=-974.24554,y=1061.08655,z=1345.67542},
+		["P2"] = {x=-1132.07324,y=1057.67249,z=1346.41235},
+		description = "You will fight against your chosen opponent in hunter."
+	},
+	["1v1 Combat Fight: Ghost Town"] = {
+		interior = 0,
+		["P1"] = {x=-350.82825,y=2250.02710,z=42.48438},
+		["P2"] = {x=-445.40240,y=2243.46851,z=42.42582},
+		description = "You will fight against your opponent in ground combat."
+	},
+	["1v1 Race: Top MC"] = {
+		interior = 0,
+		vehicle = 411,
+		["P1"] = {x=1814.05371,y=858.27032,z=10.66766},
+		["P2"] = {x=1814.05371,y=858.27032,z=10.66766},
+		race = {x=-2324.35718,y=-1626.77124,z=483.70520},
+		description = "You will race against your opponent to marked destination."
+	},
+	["1v1 Vehicle Shooter: Las Venturas Airport"] = {
+		interior = 0,
+		vehicle = 411,
+		["P1"] = {x=1461.32080,y=1808.70190,z=10.33082},
+		["P2"] = {x=1464.39734,y=1239.87939,z=10.33863},
+		jump = true,
+		shooter = true,
+		description = "You will fight against your opponent with vehicle rockets."
+	}
+}
